abf3abc0e077a314669ee0e7b3d0a06e6671f8df
yuchting <yuchting@gmail.com>
2026-04-22 13:46:25 +0800

    update chnip and gfwlist

--- TrojanPlusApp/Resources/update.sh
+++ TrojanPlusApp/Resources/update.sh
@@ -1,3 +1,3 @@
 chmod +x gfwlist2dnsmasq.sh
 ./gfwlist2dnsmasq.sh -l -o gfwlist.txt
-curl https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -o cn_mainland_ips.txt
+curl https://raw.githubusercontent.com/mayaxcn/china-ip-list/refs/heads/master/chnroute.txt -o cn_mainland_ips.txt
